e3924ec0c96199a2103a96b451c47215ea791970
airam <aaj32@alu.ua.es>
2026-02-20 10:44:08 +0100

    hola

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,4 +1,51 @@
-print('hola')
-x='adios'
+class Deporte:
+    def __init__(self, nombre, min_jugadores):
+        self.nombre = nombre
+        self.min_jugadores = min_jugadores
 
-print(x, '')
+class DeporteRaqueta(Deporte): # Así se hereda
+    def __init__(self, nombre, min_jugadores, necesita_red):
+        super().__init__(nombre, min_jugadores)
+        self.necesita_red = necesita_red
+
+
+class Tarifa:
+    def __init__(self, precio_hora):
+        self.precio_hora = precio_hora
+
+    def calcular_precio(self, horas):
+        return self.precio_hora * horas
+
+class Pista:
+    def __init__(self, id_pista, aforo_max, deporte):
+        self.id_pista = id_pista
+        self.aforo_max = aforo_max
+        self.deporte = deporte
+        self.estado = "Disponible" # Puede ser "Ocupada", "Mantenimiento"...
+
+class Evento:
+    def __init__(self, id_evento, pista, num_jugadores, prioridad):
+        self.id_evento = id_evento
+        self.pista = pista
+        self.num_jugadores = num_jugadores
+        self.prioridad = prioridad # Ej: 1 (Liga), 2 (Normal)
+
+    # SOBRECARGA: Sirve para PRIORIZAR eventos (usando el símbolo < )
+    def __lt__(self, otro_evento):
+        # Si usas evento1 < evento2, Python ejecutará esto:
+        return self.prioridad < otro_evento.prioridad
+
+class Calendario:
+    def __init__(self):
+        self.eventos = [] # Lista que guardará los eventos
+
+    def añadir_evento(self, evento):
+        # Aquí deberías usar un 'try/except' para lanzar las excepciones
+        # si se supera el aforo o choca el horario.
+        pass
+
+    # SOBRECARGA: Sirve para COMBINAR calendarios (usando el símbolo + )
+    def __add__(self, otro_calendario):
+        nuevo_calendario = Calendario()
+        nuevo_calendario.eventos = self.eventos + otro_calendario.eventos
+        return nuevo_calendario
