5dc07711bd5b37e02780695051e3502e835296cb
airam <aaj32@alu.ua.es>
2026-03-17 22:47:09 +0100

    Creamos el archivo eventos.py con clases Evento y Calendario aplicando sobrecarga de operadores. Las ponemos en el mismo archivo ya que se complementan

---
+++ eventos.py
@@ -0,0 +1,31 @@
+class Evento:
+    # Representa una reserva concreta de una pista.
+    def __init__(self, id_evento, pista, num_jugadores, prioridad):
+        self.id_evento = id_evento
+        self.pista = pista
+        self.num_jugadores = num_jugadores
+        self.prioridad = prioridad # 1 (Liga/Alta), 2 (Normal/Baja)
+
+    # Sobrecarga de operadores matemáticos: menor que (<)
+    # esto nos permite saber cómo comparar y ordenar dos objetos Evento
+    def __lt__(self, otro_evento):
+        return self.prioridad < otro_evento.prioridad
+
+
+class Calendario:
+#almacena diferentes eventos
+    def __init__(self):
+        self.eventos = []
+
+    def añadir_evento(self, evento):
+        self.eventos.append(evento)
+        # Al ordenar, se usa __lt__ de Evento
+        self.eventos.sort()
+
+    # Sobrecarga de operadores matemáticos: suma (+)
+    # Permite fusionar dos calendarios en uno nuevo combinando sus listas
+    def __add__(self, otro_calendario):
+        nuevo_calendario = Calendario()
+        nuevo_calendario.eventos = self.eventos + otro_calendario.eventos
+        nuevo_calendario.eventos.sort()
+        return nuevo_calendario
