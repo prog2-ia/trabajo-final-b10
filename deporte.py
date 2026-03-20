ff2be7b6b75106907e071c6a7c91296973a238d6
julyhr <julyhrndz32@gmail.com>
2026-03-20 11:03:26 +0100

    deporte

--- deporte.py
+++ deporte.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
-class Deporte(ABC):
+#Importamos abc para trabajar con clases abstractas
+
+class Deporte(ABC): # Clase base que define la estructura común para todos los deportes
     def __init__(self, nombre, min_jugadores):
         self.nombre = nombre
         self.min_jugadores = min_jugadores
@@ -18,28 +20,37 @@ class Deporte(ABC):
     def obtener_restricciones(self):
         pass
 
-    # Le meto esta función para imprimir los datos del deporte
+    #Utilizamos esta función para imprimir los datos del deporte
     def _str_(self):
         return f"{self.nombre} (Min: {self.min_jugadores} jug.)"
 
+
+# Especialización para deportes que usan raqueta (Tenis, Padel, etc.)
 class DeporteRaqueta(Deporte):  # Así se hereda
     def __init__(self, nombre, min_jugadores, necesita_red):
         super().__init__(nombre, min_jugadores)
         self.necesita_red = necesita_red
 
     def obtener_restricciones(self):
-        estado_red = "con red" if self._necesita_red else "sin red"
+        # Genera un mensaje dinámico basado en si el deporte requiere red o no
+        estado_red = "con red" if self.necesita_red else "sin red"
         return f"Obligatorio traer raquetas propias. Se juega {estado_red}."
 
+
+
+# Especialización para deportes colectivos (Fútbol, Baloncesto, etc.)
 class DeporteEquipo(Deporte):
     def __init__(self, nombre, min_jugadores, duracion_oficial):
         super().__init__(nombre, min_jugadores)
         self.duracion_oficial = duracion_oficial
 
     def obtener_restricciones(self):
-        return f"Equipos uniformados. Partidos oficiales de {self._duracion_oficial} minutos."
+        #Genera un mensaje diciendo los minutos que duran los partidos de ese deporte
+        return f"Equipos uniformados. Partidos oficiales de {self.duracion_oficial} minutos."
 
 
+
+# Clase para deportes que no encajan en las categorías anteriores
 class Deporte_otro(Deporte):
     def __init__(self, nombre, min_jugadores):
         super().__init__(nombre, 1)
